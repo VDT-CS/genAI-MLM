eac66bb2fe509cbd84855515c349773b2e2b110c
magniswerfer <magnushoholt@pm.me>
2024-06-26 22:25:57 -0400

    added a random personality prompt

--- main.py
+++ main.py
@@ -13,23 +13,27 @@ callbacks = {
         "SCAN": lambda: on_input.perform_scan("scanned_image.jpg", scanner_printer, gui),
         "GENERATE": lambda: on_input.send_to_replicate("scanned_image.jpg", replicate, gui, scanner_printer),
         "STYLE": {
+            "NONE": lambda: on_input.add_string_to_append_dict("STYLE", ""),
             "PHOTO": lambda: on_input.add_string_to_append_dict("STYLE", "Rendered as a high-resolution photo by a Canon 5D Mark IV"),
             "DIGITAL_ART": lambda: on_input.add_string_to_append_dict("STYLE","In the style of Deviant Art's digital 3D art"),
             "ANIME": lambda: on_input.add_string_to_append_dict("STYLE", "Anime-inspired, with detailed pencil and cross-hatching")
         },
         "TIME": {
+            "NONE": lambda: on_input.add_string_to_append_dict("TIME", ""),
             "CURRENT": lambda: on_input.add_string_to_append_dict("TIME", "Set in the present days"),
             "FUTURE": lambda: on_input.add_string_to_append_dict("TIME", "Imagined in a speculative, futuristic era"),
             "PAST": lambda: on_input.add_string_to_append_dict("TIME", "Rooted in historical times")
         },
         "TONE":{
-            "NONE": lambda: on_input.add_string_to_append_dict("TONE", "Neutral tone, no thematic emphasis"),
+            "NONE": lambda: on_input.add_string_to_append_dict("TONE", ""),
+            "NEUTRAL": lambda: on_input.add_string_to_append_dict("TONE", "Neutral tone, no thematic emphasis"),
             "CRITICAL": lambda: on_input.add_string_to_append_dict("TONE", "Serious, introspective themes"),
             "CARING": lambda: on_input.add_string_to_append_dict("TONE", "Infused with a caring and nurturing tone"),
             "WHIMSICAL": lambda: on_input.add_string_to_append_dict("TONE", "Whimsical, playful, and light-hearted")
         },
         "BACKGROUND":{
-            "NONE": lambda: on_input.set_remove_background(lambda: on_input.add_string_to_append_dict("BACKGROUND", "Isolated on a white background!", "Exclude colors, shadows, and textures in the background")),
+            "NONE": lambda: on_input.add_string_to_append_dict("BACKGROUND",""),
+            "WHITE": lambda: on_input.set_remove_background(lambda: on_input.add_string_to_append_dict("BACKGROUND", "Isolated on a white background!", "Exclude colors, shadows, and textures in the background")),
             "CITY": lambda: on_input.add_string_to_append_dict("BACKGROUND","Background overlooking a vibrant, lit-up cityscape"),
             "FOREST": lambda: on_input.add_string_to_append_dict("BACKGROUND","Background within a lively, verdant forest")
         }
--- replicate_communication.py
+++ replicate_communication.py
@@ -1,4 +1,5 @@
 import os
+import random
 import replicate
 from dotenv import load_dotenv
 from urllib.request import urlopen
@@ -21,7 +22,7 @@ class Replicate:
 
         with open(file_path, 'rb') as sketch_file_object:
             prediction = deployment.predictions.create(input={"image": sketch_file_object, 
-                                                              "prompt": user_prompt,
+                                                              "prompt": self.random_personality_prompt + " " + user_prompt,
                                                               "negative_prompt": standard_negative_prompt + negative_prompt
                                                               })
         prediction.wait()
@@ -43,6 +44,28 @@ class Replicate:
     
     def append_to_prompt(self, prompt):
         self.appended_prompt += prompt
+
+    # A function that randomly adds one of several strings to the user prompt
+    def random_personality_prompt():
+        personality_promts = [
+                            "",
+                            "No matter what, the following image should be contain sausages:", 
+                            "Try to make the image look like a cat:", 
+                            "Be sure to include a rainbow in the image:", 
+                            "If in doubt, add a tree to the image:", 
+                            "Everything is better with a smiley face:", 
+                            "The image should be a little bit spooky:", 
+                            "Everything is made of cheese:", 
+                            "If there is a person, they should be an alien:", 
+                            "the image should be a little bit glitchy:", 
+                            "Add extra limbs to the image:",
+                            "The image should be a little bit abstract:",
+                            "Add extra eyes to the image:",
+                            "Add a lot of eyes to the image:",
+                            "All colours should be inverted:",
+                            "Only use the color red:",
+                            ]
+        return personality_promts[random.randint(0, len(personality_promts) - 1)]
     
 # For debugging purposes
 if __name__ == "__main__":
